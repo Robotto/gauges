add6016e0eb87d0b2f3a976571f2be613328d599
robotto <mark@moore.dk>
2019-03-19 23:04:11 +0100

    ready for test on pi

--- gaugesServer.py
+++ gaugesServer.py
@@ -2,12 +2,12 @@
 import datetime, time
 import socket
 from IPy import IP
+import subprocess
 
 
+print (time.ctime(),"startup!")
 
-print time.ctime(),"startup!"
-
-TCP_IP = '192.168.0.179'
+TCP_IP = '192.168.0.145'
 TCP_PORT = 9999
 BUFFER_SIZE = 20  # Normally 1024, but we want fast response
 
@@ -16,21 +16,33 @@ s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) #make socket reuseable, 
 s.bind((TCP_IP, TCP_PORT))
 s.listen(1)
 
-print 'Opening TCP port', TCP_PORT
+
+print('Opening TCP port', TCP_PORT)
 
 while True:
     try:
         conn, addr = s.accept()
-        print time.ctime(),'Connection from:', addr
+        print(time.ctime(),'Connection from:', addr)
+        #ask sardukar about disk usage
+        sardukarDiskUsage = subprocess.check_output("ssh robotto@sardukar.moore.dk -p 222 df | grep 'vg00-root' | awk '{print $5}'", shell=True)
+        sardukarPercent = int(sardukarDiskUsage.strip().replace("%", ""))
+        print('Sardukar:',sardukarPercent)
+
+        # read rocksteady's disk usage from file:
+        with open('rocksteadyReport', 'r') as rocksteadyReportFile:
+            for line in rocksteadyReportFile:
+                rocksteadyPercent = int(line.strip().replace("%", ""))
+                print('Rocksteady:',rocksteadyPercent)
+
         #txString = str(22)
-        txString = str(int(a.moon_phase(datetime.datetime.now())))+'\r'
+        txString = str(sardukarPercent)+,str(rocksteadyPercent)+'\r'
         conn.send(txString)#+'\n')  # echo
-        print 'TX: ', txString #txString.replace('\r','\r\n')
-        print 'Client disconnected.'
-        print '--------------------------'
+        print ('TX: ', txString) #txString.replace('\r','\r\n')
+        print ('Client disconnected.')
+        print ('--------------------------')
         conn.close()
     except Exception as e:
         #print "hmm.. It looks like there was an error: " + str(e)
-        print 'Client disconnected... :',str(e)
-        print '--------------------------'
+        print ('Client disconnected... :',str(e))
+        print ('--------------------------')
         conn.close()
