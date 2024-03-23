b68d0fcf3b24f369cc85e338dcd06a93660d114e
robotto <mark@moore.dk>
2024-03-23 22:33:31 +0100

    replacing sadukar with orion

--- gaugesServer.py
+++ gaugesServer.py
@@ -16,7 +16,8 @@ s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) #make socket reuseable, 
 s.bind((TCP_IP, TCP_PORT))
 s.listen(1)
 
-sardukarPercent=0
+#sardukarPercent=0
+orionPercent=0
 rocksteadyPercent=0
 
 print('Opening TCP port', TCP_PORT)
@@ -25,10 +26,14 @@ while True:
     try:
         conn, addr = s.accept()
         print(time.ctime(),'Connection from:', addr)
-        #ask sardukar about disk usage
-        sardukarDiskUsage = subprocess.check_output("ssh sardukar df | grep 'vg00-root' | awk '{print $5}'", shell=True)
-        sardukarPercent = int(sardukarDiskUsage.strip().replace("%", ""))
-        print('Sardukar:',sardukarPercent)
+#        #ask sardukar about disk usage
+#        sardukarDiskUsage = subprocess.check_output("ssh sardukar df | grep 'vg00-root' | awk '{print $5}'", shell=True)
+#        sardukarPercent = int(sardukarDiskUsage.strip().replace("%", ""))
+#        print('Sardukar:',sardukarPercent)
+        #ask orion about disk usage
+        orionDiskUsage = subprocess.check_output("ssh orion df | grep '/dev/md127' | awk '{print $5}'", shell=True)
+        orionPercent = int(orionDiskUsage.strip().replace("%", ""))
+        print('Orion:',orionPercent)
 
         # read rocksteady's disk usage from file:
         with open('rocksteadyReport', 'r') as rocksteadyReportFile:
@@ -37,7 +42,7 @@ while True:
                 print('Rocksteady:',rocksteadyPercent)
 
         #txString = str(22)
-        txString = str(sardukarPercent)+'\r'+str(rocksteadyPercent)+'\r'
+        txString = str(orionPercent)+'\r'+str(rocksteadyPercent)+'\r'
         conn.send(txString)#+'\n')  # echo
         print ('TX: ', txString) #txString.replace('\r','\r\n')
         print ('Client disconnected.')
