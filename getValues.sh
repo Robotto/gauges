b0a01aa64d21dc97b7126787dc4e86d96ca29115
robotto <mark@moore.dk>
2024-03-23 22:29:41 +0100

    added orion to testscript

--- getValues.sh
+++ getValues.sh
@@ -2,8 +2,12 @@
 
 #rocksteady:
 echo "Rocksteady:"
-ssh robotto@rocksteady.moore.dk -p 222 df | grep 'lv' | awk '{print $5}'
+ssh rocksteady df | grep 'lv' | awk '{print $5}'
 
 #sardukar:
 echo "Sardukar:"
-ssh robotto@sardukar.moore.dk -p 222 df | grep 'vg00-root' | awk '{print $5}'
+ssh sardukar df | grep 'vg00-root' | awk '{print $5}'
+
+#Orion:
+echo "Orion:"
+ssh orion df | grep '/dev/md127' | awk '{print $5}'
